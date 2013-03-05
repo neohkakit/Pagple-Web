cdd89575107678b14705147c736ce8c7494615c1
Ryne Cheow <rynec92@hotmail.com>
2013-03-05 18:49:07 +0800

    main page loading

---
+++ Website/index-old.php
@@ -0,0 +1,263 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Welcome! Future Home of Another Amazing Website Powered by Exabytes</title>
+<style>
+/*Base class*/
+body {
+	color: #333;
+	background:#FFFFFF url(http://www.exabytes.com.my/welcome/images/bg.gif) repeat left top;
+	font: 12px "Arial", Helvetica, sans-serif;
+	
+	margin:0 auto;
+}
+.cloud {
+	background:transparent url(http://www.exabytes.com.my/welcome/images/footer-cloud.png) no-repeat center bottom;
+	width:977px;
+	height:223px;
+	display:block;
+	position:absolute;
+	bottom:0;
+	text-align:center;
+	margin-left:-490px;
+	left:50%;
+}
+/* Main structure classes */
+.wrapper {
+	float:left;
+	width:100%;
+	height:380px;
+}
+.box {
+	position:absolute;
+	top:45%;
+	left:50%;
+	width:880px;
+	height:400px;
+	display:block;
+	margin:-200px 0 0 -430px;
+	text-align:center;
+}
+.left {
+	float:left;
+	width:430px;
+	border-right:1px solid #CCC;
+	text-align:right;
+	padding:20px 30px 20px 20px;
+	min-height:230px;
+}
+.right {
+	float:right;
+	width:249px;
+	text-align: left;
+	padding:20px;
+	min-height:230px;
+}
+.logo {
+	margin:0px 0 10px 0;
+}
+.content {
+	padding-top:20px;
+}
+.footer {
+	position:absolute;
+	bottom:0;
+	left:50%;
+	margin-left:-390px;
+	width:780px;
+	font-size:11px;
+	text-align:center;
+	padding:5px 0;
+	z-index:0;
+	
+}
+.footer p {
+	color:#333;
+	margin:10px 0px;
+	line-height:130%;
+}
+.ad {
+	margin-top:450px;
+	text-align:center;
+}
+/*Headings*/
+h1 {
+	margin: 10px 0px 10px 0px;
+	font-weight: normal;
+	font-size: 16px;
+	color:#FFFFFF;
+}
+
+h2 {
+	margin: 0px 0px 10px 0px;
+	font-weight: normal;
+	font-size: 18px;
+	color: #FF9900;
+}
+h1, h2 {
+	-moz-font-feature-settings: normal;
+	-moz-font-language-override: normal;
+	-x-system-font: none;
+	font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+	font-size-adjust: none;
+	font-stretch: normal;
+	font-style: normal;
+	font-variant: normal;
+	font-weight: 300;
+	line-height: 120%;
+	text-transform:capitalize;
+}
+/*HTML defaults*/
+blockquote {
+	padding:0;
+	margin:0;
+	font-style: italic;
+	font-size:1.6em;
+	color:#fff;
+	line-height: 1.4em;
+}
+ul.btns {
+list-style:none;
+text-align:center;
+padding-top:20px;
+}
+ul.btns li {
+	float:left;
+	margin:0px 6px;
+}
+ul.btns li.btn01 a {
+	display:block;
+	width:120px;
+	height:138px;
+	background:transparent url(http://www.exabytes.com.my/welcome/images/btn01.png) no-repeat top left;
+	text-indent:-99999px;
+}
+ul.btns li.btn01 a:hover {
+	display:block;
+	width:120px;
+	height:138px;
+	background:transparent url(http://www.exabytes.com.my/welcome/images/btn01.png) no-repeat bottom left;
+	text-indent:-99999px;
+}
+ul.btns li.btn02 a {
+	display:block;
+	width:120px;
+	height:138px;
+	background:transparent url(http://www.exabytes.com.my/welcome/images/btn02.png) no-repeat top left;
+	text-indent:-99999px;
+}
+ul.btns li.btn02 a:hover {
+	display:block;
+	width:120px;
+	height:138px;
+	background:transparent url(http://www.exabytes.com.my/welcome/images/btn02.png) no-repeat bottom left;
+	text-indent:-99999px;
+}
+ul.btns li.btn03 a {
+	display:block;
+	width:120px;
+	height:138px;
+	background:transparent url(http://www.exabytes.com.my/welcome/images/btn03.png) no-repeat top left;
+	text-indent:-99999px;
+}
+ul.btns li.btn03 a:hover {
+	display:block;
+	width:120px;
+	height:138px;
+	background:transparent url(http://www.exabytes.com.my/welcome/images/btn03.png) no-repeat bottom left;
+	text-indent:-99999px;
+}
+ul.btns li.btn04 a {
+	display:block;
+	width:120px;
+	height:138px;
+	background:transparent url(http://www.exabytes.com.my/welcome/images/btn04.png) no-repeat top left;
+	text-indent:-99999px;
+}
+ul.btns li.btn04 a:hover {
+	display:block;
+	width:120px;
+	height:138px;
+	background:transparent url(http://www.exabytes.com.my/welcome/images/btn04.png) no-repeat bottom left;
+	text-indent:-99999px;
+}
+ul.btns li.btn05 a {
+	display:block;
+	width:120px;
+	height:138px;
+	background:transparent url(http://www.exabytes.com.my/welcome/images/btn05.png) no-repeat top left;
+	text-indent:-99999px;
+}
+ul.btns li.btn05 a:hover {
+	display:block;
+	width:120px;
+	height:138px;
+	background:transparent url(http://www.exabytes.com.my/welcome/images/btn05.png) no-repeat bottom left;
+	text-indent:-99999px;
+}
+ul.btns li.btn06 a {
+	display:block;
+	width:120px;
+	height:138px;
+	background:transparent url(http://www.exabytes.com.my/welcome/images/btn06.png) no-repeat top left;
+	text-indent:-99999px;
+}
+ul.btns li.btn06 a:hover {
+	display:block;
+	width:120px;
+	height:138px;
+	background:transparent url(http://www.exabytes.com.my/welcome/images/btn06.png) no-repeat bottom left;
+	text-indent:-99999px;
+}
+a:link, a:visited {
+	color:#333333;
+	text-decoration:underline;
+}
+a:hover {
+	text-decoration:none;
+}
+p {
+	margin:0px;
+	padding:0px;
+	line-height:150%;
+}
+/*Image classes*/
+img.icon {
+	float:left;
+	padding:5px 10px 0 0;
+}
+img.small_icon {
+	float:right;
+	padding:5px;
+}
+</style>
+</head>
+<body>
+<div class="wrapper">
+<div class="box">
+<img src="http://www.exabytes.com.my/welcome/images/logo.png" style="display:block; position:absolute; left:0px; top:-70px;"/><img src="http://www.exabytes.com.my/welcome/images/welcome-title.png" />
+<h1>FUTURE HOME OF ANOTHER AMAZING WEBSITE POWERED BY EXABYTES</h1>
+<p><strong>If you are visiting this site</strong>, we serve to inform you that this site is currently under construction and do visit again in future or you can contact the honorable webmaster for further information. <strong>If you are the webmasters of this site</strong>, Congratulations! Your online storefront is readily waiting for you to move yet another step forward. For your convenience, kindly click on the below icons which bring you to technical support, login, tutorials, etc.</p>
+<ul class="btns">
+<li class="btn01"><a href="https://billing.exabytes.com.my/mypanel/clientarea.php">Client Area</a></li>
+<li class="btn02"><a href="https://support.exabytes.com/tutorials/">Video Tutorials</a></li>
+<li class="btn03"><a href="https://support.exabytes.com/">24x7 Support</a></li>
+<li class="btn04"><a href="https://support.exabytes.com/KB/search.aspx">Knowledge Base</a></li>
+<li class="btn05"><a href="https://support.exabytes.com/News/news.aspx">Updated News</a></li>
+<li class="btn06"><a href="http://<?php echo $_SERVER['HTTP_HOST'];?>:2095">Webmail Login</a></li>
+</ul>
+
+</div>
+  <div class="cloud">
+    <div class="footer">
+      <p>Visit Exabytes' Official Website: <a href="http://www.exabytes.com.my">www.exabytes.my</a> | Contact Sales: <a href="mailto:sales@exabytes.com.my">sales@exabytes.com.my</a></p>
+      <p>This future website is hosted and powered by <a href="http://www.exabytes.com.my">Exabytes.com.my</a>, the specialist in Linux and Windows Web Hosting since 2001.<br />
+        We also support Web 2.0 technologies, Ruby on Rails, php, asp.NET, asp, ColdFusion, web publishing and other advanced hosting needs.<br />
+        Please <a href="http://exabytes.com.my/about/contact/form.php">contact us</a> for more details as we are here to serve you around the clock.</p>
+      <p style="text-transform:uppercase">Copyright &copy; 2001-2011 Exabytes Network Sdn Bhd (576092-T). All Trademarks Are The Property of Their Respective Owners.</p>
+    </div>
+  </div>
+</div>
+</body>
+</html>
---
+++ Website/php/database.php
@@ -0,0 +1,142 @@
+<?php
+	/**
+	 * Interface for class Database. Add or alter the functions here accordingly.
+	*/
+	interface DatabaseTemplate {
+		// public function
+		public function connect();
+		public function close();
+		public function execute($query);
+	}
+	
+	/**
+	 * PHP mysql API is deprecated. I had revised my code on using mysqli API (MySQL Improved Extension).
+	 * Make sure your PHP version is up-to-date. Recommended version 5.2 or above.
+	 * read the DOC for more information: http://php.net/manual/en/mysqli.overview.php
+	 *
+	 * All Rights Reserved
+	 *
+	 * Lim Teik Wei @ d4vid.tw89@gmail.com
+	*/
+	class Database implements DatabaseTemplate
+	{
+		// private attributes
+		private $user;
+		private $password;  
+		private $database; // database name
+		private $host;     // database host
+		private $con;      // database connection
+		private $tid;      // thread id		
+		private $result;   // result retrieve from table
+		private $connected = FALSE; // check database connection
+		
+		// constructor
+		public function __construct() {
+			$this->host      = 'localhost';
+			$this->user      = 'pagpleco_admin';
+			$this->password  = 'PagpleAdmin@hotmail.com';
+			$this->database  = 'pagpleco_subscriber';
+		}
+		
+		// destructor
+		function __destruct() {
+			// auto garbage collected, perform something only if necessary
+		}
+		
+		// connect to mysql
+		public function connect() {	
+			// connect only if not connected
+			if (!$this->connected) {
+				$this->con = new mysqli($this->host, $this->user , $this->password, $this->database);			
+				
+				// connect_errno only works on php 5.3 or above, use php connect_error if below.
+				if ($this->con->connect_errno) {
+					// echo 'Couldn\'t connect database (Error ' . $this->con->connect_errno . '): ' . $this->con->connect_error();
+					exit();
+				}
+				
+				// connection thread id
+				$this->tid = $this->con->thread_id;
+				
+				// assign TRUE if connected, else FALSE
+				$this->connected = $this->con->ping()? TRUE : FALSE;				
+			} else {
+				// connected, do nothing
+			}
+		}
+		
+		// close database connection
+		public function close() {
+			if ($this->connected) {
+				// kill the connection thread before closing connection
+				$this->con->kill($this->con->thread_id);
+				$this->con->close();
+				$this->connected = FALSE;
+			} else {
+				// connection closed or not connected
+			}
+		}
+		
+		// execute query
+		public function execute($query) {
+			if ($this->connected) {
+				if (!($this->result = $this->con->query($query, MYSQLI_USE_RESULT))) {
+					return NULL;	
+				}
+			} else {
+				// no connection to database.
+				return NULL;
+			}
+			return $this->result;
+		}
+		
+		// print mysql statement error message
+		private function print_err($statement, $errmsg) {
+			echo $errmsg . ' (' . $statement->errno . '): ' . $statement->error;
+		}
+		
+		/**
+		 * Temporary section
+		 * : Remove function accordingly if not part of the development
+		*/
+		
+		// inserting email using mysqli_prepare
+		public function temp_execute($query, $email) {
+			if ($this->connected) {
+				if ($statement = $this->con->prepare($query)) {
+					if (!$statement->bind_param('s', $email)) {
+						// print_err($statement, 'Binding parameter failed');
+						return FALSE;
+					}					
+					if(!$statement->execute()) {
+						// print_err($statement, 'Execution failed');
+						return FALSE;
+					}
+					$statement->close();
+				} else {
+					// query error
+					return FALSE;
+				}
+			} else {
+				// no connection to database.
+				return FALSE;
+			}
+			return TRUE;
+		}
+	} // end class
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
